54f97274c8b8c4bf43cfbb5dc183868054bda0b0
Budes Alin <alin76alin@yahoo.com>
2020-04-25 20:27:47 +0300

    implements user and friendship functionality

--- user-service/src/main/java/com/thejavacademy/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/thejavacademy/userservice/UserServiceApplication.java
@@ -13,52 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import javax.management.relation.RelationType;
 
 @SpringBootApplication
-public class UserServiceApplication implements CommandLineRunner {
-
-	MySqlUserRepo userRepo;
-	MySqlFriendshipRepository friendshipRepo;
-	UserServiceImpl userService;
-
-	public UserServiceApplication(MySqlUserRepo userRepo, MySqlFriendshipRepository friendshipRepo, UserServiceImpl userService) {
-		this.userRepo = userRepo;
-		this.friendshipRepo = friendshipRepo;
-		this.userService = userService;
-	}
-
+public class UserServiceApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		Friendship f1 = new Friendship();
-		f1.setId("id123");
-		f1.setUserOneId("1");
-		f1.setUserTwoId("2");
-		f1.setRelationshipStatus(FriendshipType.FRIENDS);
-		f1.setActionUserId(1);
-
-		Friendship f2 = new Friendship();
-		f2.setId("id456");
-		f2.setUserOneId("2");
-		f2.setUserTwoId("4");
-		f2.setRelationshipStatus(FriendshipType.FRIENDS);
-		f2.setActionUserId(1);
-
-		Friendship f3 = new Friendship();
-		f3.setId("id789");
-		f3.setUserOneId("2");
-		f3.setUserTwoId("4");
-		f3.setRelationshipStatus(FriendshipType.FRIENDS);
-		f3.setActionUserId(2);
-
-//		friendshipRepo.save(f1);
-//		friendshipRepo.save(f2);
-//		friendshipRepo.save(f3);
-
-		SearchUserResponse response = userService.getFriends("b");
-		//response.getUsers().forEach(System.out::println);
-		System.out.println(response);
 	}
 }
